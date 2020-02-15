9b5ddab4bb24162c59dccc236ad308a5e6f36aca
admin <rakdav@inbox.ru>
2020-02-15 18:46:04 +0200

    sec

--- app/src/main/java/com/example/graph2/MyDraw.java
+++ app/src/main/java/com/example/graph2/MyDraw.java
@@ -6,10 +6,16 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MyDraw extends View
 {
+    private int x;
+    private int y;
+    private Canvas canvas;
+    Paint paint;
     public MyDraw(Context context) {
         super(context);
     }
@@ -17,7 +23,8 @@ public class MyDraw extends View
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint paint=new Paint();
+        this.canvas=canvas;
+        paint=new Paint();
         paint.setSubpixelText(true);
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.FILL);
@@ -35,6 +42,25 @@ public class MyDraw extends View
         canvas.drawText("My Canvas",200,100,paint);
         Bitmap bitmap= BitmapFactory.decodeResource(getResources(),
                 R.drawable.ic_launcher_background);
-        canvas.drawBitmap(bitmap,150,150,paint);
+        canvas.drawBitmap(bitmap,x,y,paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int eventAction = event.getAction();
+        if (eventAction == MotionEvent.ACTION_DOWN)  {
+            x=(int)event.getX();
+            y=(int)event.getY();
+            update();
+        }
+        return true;
+    }
+    protected void update () {
+        paint.setColor(getResources().getColor(R.color.colorAccent));
+        canvas.drawPaint(paint);
+        Bitmap bitmap= BitmapFactory.decodeResource(getResources(),
+                R.drawable.ic_launcher_background);
+        canvas.drawBitmap(bitmap,x,y,paint);
+        invalidate();
     }
 }
